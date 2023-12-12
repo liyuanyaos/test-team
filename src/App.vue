ea7fbf42fb08b109d56da602e843f87e056e3ef8
mz <2576307149@qq.com>
2023-12-12 14:53:18 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to 李帅"/>
+    <HelloWorld msg="Welcome to 黄金坤疤哥"/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
